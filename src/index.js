7efb92d8beef04ee3eba0304e6f92494fd9d3bf4
Cornelius Soon <cornelius.soon@gmail.com>
2024-01-06 11:09:40 +1300

    Initialise project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,47 @@
 (() => {
+    const unsecureWeatherApiKey = "25108cb167ef437d86b204741240201";
+    const fetchWeatherApi =
+        "https://api.weatherapi.com/v1/current.json?key=25108cb167ef437d86b204741240201&q=Hamilton New Zealand&aqi=no";
+
+    function getLocationInformation(locationDataObject) {
+        const obj = {};
+
+        obj.name = locationDataObject.name;
+        obj.country = locationDataObject.country;
+        obj.timezone = locationDataObject.tz_id;
+        obj.localtime = locationDataObject.localtime;
+
+        return obj;
+    }
+
+    function getCurrentInformation(currentDataObject) {
+        const obj = {};
+
+        obj.lastUpdated = currentDataObject.last_updated;
+        obj.temperature = currentDataObject.temp_c;
+        obj.conditionText = currentDataObject.condition.text;
+        obj.conditionIcon = currentDataObject.condition.icon;
+        obj.windDirection = currentDataObject.wind_dir;
+        obj.windSpeed = currentDataObject.wind_kph;
+        obj.humidity = currentDataObject.humidity;
+        obj.precipitation = currentDataObject.precip_mm;
+
+        return obj;
+    }
+
+    async function getWeatherInformation(searchTerm) {
+        const url = `https://api.weatherapi.com/v1/current.json?key=25108cb167ef437d86b204741240201&q=${searchTerm}`;
+
+        const fetchResponse = await fetch(url, { mode: "cors" });
+        const responseBodyJSON = await fetchResponse.json();
+        return responseBodyJSON;
+    }
+
+    getWeatherInformation("Auckland, New Zealand").then((jsonData) => {
+        console.log(jsonData);
+        console.log(getLocationInformation(jsonData.location));
+        console.log(getCurrentInformation(jsonData.current));
+    });
 
     return {};
 })();
